d587b07a3c3d1a8d0b7906ade1ed4767bbd221a3
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-29 13:25:04 +0200

    add firebase authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  auth,
+  logInWithGoogle,
+  logout,
+} from "../services/firebaseAuthentication";
 
 const Navbar = () => {
-  return <div className="navbar-container">Catdex</div>;
+  const [user, loading] = useAuthState(auth);
+
+  return (
+    <div className="navbar-container">
+      <div>Catdex</div>
+      <div>
+        {!loading && user && user?.photoURL ? (
+          <button onClick={logout}>
+            <img src={user.photoURL} alt="user" className="rounded w-8 h-8" />
+          </button>
+        ) : (
+          <button onClick={logInWithGoogle}>Login</button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/types/userTypes.d.ts
@@ -0,0 +1,7 @@
+export type UserType = {
+  uid: string;
+  email: string;
+  name: string;
+  authProvider: string;
+  caughtCats: any;
+};
